d3b5d122e34b754c8808ca6afbe98b8425a6d185
joseenriquemg <josee.munoz@iesnervion.es>
2024-11-15 14:42:25 +0100

    Hola

--- Tema_3_Ejercicios/src/bidimensionalesboletin3/Ejercicio02.java
+++ Tema_3_Ejercicios/src/bidimensionalesboletin3/Ejercicio02.java
@@ -7,28 +7,80 @@ public class Ejercicio02 {
 	public static void main(String[] args) {
 
 		// Variable para recoger la tabla del usuario.
-		int tabla[][] = new int[5][6];
+		double tabla[][] = new double[4][5];
+
+		// Creamos una variable para recoger el valor minimo.
+		double min;
+
+		// Creamos una variable para recoger el valor maximo.
+		double max;
+
+		// Creamos una variable para la media.
+		double media = 0;
+
+		// Variable para sumar las notas.
+		double suma = 0;
 
 		// Creamos un nuevo Scanner.
 		Scanner sc = new Scanner(System.in);
 
-		// Solicitamos las notas al usaurio.
-		for (int filas = 1; filas < tabla.length; filas++) {
+		// Creamos un primer bucle for para solicitar los datos.
+		for (int filas = 0; filas < tabla.length; filas++) {
 
-			System.out.println("Nota " + filas);
+			for (int columnas = 0; columnas < tabla[filas].length; columnas++) {
 
-			for (int columnas = 1; columnas < tabla.length; columnas++) {
+				System.out.println(
+						"Introduzca la nota del Alumno " + (filas + 1) + " en la asignatura " + (columnas + 1));
 
-				System.out.println();
+				tabla[filas][columnas] = sc.nextInt();
 
 			}
 
 		}
 
-		// Bucle para las columnas.
+		System.out.print("               ");
+		System.out.print("Asignatura 1   ");
+		System.out.print("Asignatura 2   ");
+		System.out.print("Asignatura 3   ");
+		System.out.print("Asignatura 4   ");
+		System.out.print("Asignatura 5   ");
+
 		for (int filas = 0; filas < tabla.length; filas++) {
 
-			System.out.println("Alumno " + (filas + 1) + "\t");
+			suma = 0;
+			min = 0;
+			max = 0;
+
+			max = Integer.MAX_VALUE;
+			min = Integer.MIN_VALUE;
+
+			System.out.println();
+
+			System.out.print("Alumno " + (filas + 1));
+
+			for (int columnas = 0; columnas < tabla[filas].length; columnas++) {
+
+				System.out.print("            " + tabla[filas][columnas]);
+
+				suma += tabla[filas][columnas];
+
+				if (tabla[filas][columnas] > min) {
+
+					min = tabla[filas][columnas];
+
+				}
+
+				if (tabla[filas][columnas] < max) {
+
+					max = tabla[filas][columnas];
+
+				}
+
+			}
+
+			System.out.print("       Maximo: " + min + "   Minimo: " + max + "    Media: " + media);
+
+			media = suma / 5;
 
 		}
 
---
+++ Tema_3_Ejercicios/src/funciones/Ejercicio01.java
@@ -0,0 +1,17 @@
+package funciones;
+
+public class Ejercicio01 {
+
+	public static void main(String[] args) {
+		
+		
+
+	}
+	
+	static void suma(int[] tabla) {
+		
+		int numeros
+		
+	}
+
+}
