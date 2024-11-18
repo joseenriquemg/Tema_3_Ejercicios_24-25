7ecbf050150c504f263e94436d919d155b6b165d
joseenriquemg <josee.munoz@iesnervion.es>
2024-11-18 12:45:41 +0100

    Ejercicio 1

--- Tema_3_Ejercicios/src/funciones/Ejercicio01.java
+++ Tema_3_Ejercicios/src/funciones/Ejercicio01.java
@@ -1,17 +1,50 @@
 package funciones;
 
+import java.util.Scanner;
+
 public class Ejercicio01 {
 
 	public static void main(String[] args) {
+
+		int numeros;
 		
+		int tabla[];
 		
+		int longitud;
+		
+		Scanner sc = new Scanner(System.in);
+			
+		System.out.println("Defina la longitud del array:");
+		
+		longitud = sc.nextInt();
+		
+		tabla = new int[longitud];
+		
+		for (int i = 0; i < tabla.length; i++) {
+			
+			System.out.println("Introduzca los valores:");
+			
+			numeros = sc.nextInt();
+			
+			tabla[i] = numeros;
+		}
+		
+		System.out.println(suma(tabla));
 
 	}
-	
-	static void suma(int[] tabla) {
+
+	static int suma(int[] tabla) {
+
+		int sumaTotal = 0;
+
+		for (int i = 0; i < tabla.length; i++) {
+			
+			sumaTotal += tabla[i];
 		
-		int numeros
+		}
 		
+		return sumaTotal;
+
 	}
 
 }
